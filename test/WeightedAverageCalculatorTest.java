718ce7928c7621b5a4ead258bac02e996f414824
john <johnotters@yahoo.com>
2019-11-17 18:38:41 -0800

    Adds skeleton for weighted average test class

---
+++ test/WeightedAverageCalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeightedAverageCalculatorTest {
+    @Test
+    public void test_correctlyCalculatesAverage() {
+        assert false;
+    }
+}
