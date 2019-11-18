c7d22cfa98357aaa2f8744921858a2a42719ab01
john <johnotters@yahoo.com>
2019-11-17 17:42:31 -0800

    Initial Commit

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Student {
+    public Student(String name) {
+
+    }
+
+    public void addAssignmentScore(double as) {
+
+    }
+
+    public void addExamScore(double es) {
+
+    }
+
+    private ArrayList<Double> assignmentScores = new ArrayList<>();
+    private ArrayList<Double> examScores = new ArrayList<>();
+    private String name;
+}
