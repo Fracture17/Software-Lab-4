6970093db5cc411996df83bcb9608ac042745e38
john <johnotters@yahoo.com>
2019-11-17 18:37:01 -0800

    Adds skeleton for weighted average calculator

---
+++ src/WeightedAverageCalculator.java
@@ -0,0 +1,6 @@
+public class WeightedAverageCalculator implements AverageCalculator {
+    @Override
+    public double calc(Iterable<Double> assignmentScores, Iterable<Double> examScores) {
+        return 0;
+    }
+}
