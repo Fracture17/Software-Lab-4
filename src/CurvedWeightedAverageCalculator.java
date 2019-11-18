03c1ab85d21927130bc8613d86b4da40252fd7ea
john <johnotters@yahoo.com>
2019-11-17 19:10:36 -0800

    Implements calc method by overloading computeAssignmentAverage

--- src/CurvedWeightedAverageCalculator.java
+++ src/CurvedWeightedAverageCalculator.java
@@ -1,3 +1,12 @@
-public class CurvedWeightedAverageCalculator extends WeightedAverageCalculator {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class CurvedWeightedAverageCalculator extends WeightedAverageCalculator {
+    @Override
+    protected double computeAssignmentAverage(ArrayList<Double> assignmentScores) {
+        if(assignmentScores.size() >= 2) {
+            assignmentScores.remove(Collections.min(assignmentScores));
+        }
+        return super.computeAssignmentAverage(assignmentScores);
+    }
 }
