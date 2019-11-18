e6f54de320163df3b6cf0fc436ec459dc5ac573a
john <johnotters@yahoo.com>
2019-11-17 18:25:13 -0800

    Implements methods

--- src/Roster.java
+++ src/Roster.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 
 public class Roster {
     public Roster(String name, String number) {
-
+        this.courseName = name;
+        this.courseNumber = number;
     }
 
     public String getCourseName() {
-
+        return courseName;
     }
 
     public void addStudent(Student student) {
-
+        students.add(student);
     }
 
     String courseName;
