5079f337d0bca5b46192ec174c16f8814a1985a8
john <johnotters@yahoo.com>
2019-11-17 18:23:53 -0800

    Adds basic Roster skeleton

---
+++ src/Roster.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Roster {
+    public Roster(String name, String number) {
+
+    }
+
+    public String getCourseName() {
+
+    }
+
+    public void addStudent(Student student) {
+
+    }
+
+    String courseName;
+    String courseNumber;
+    ArrayList<Student> students = new ArrayList<>();
+}
