d8cac876acbc51a2e496e84f9b2fc5712c186d40
john <johnotters@yahoo.com>
2019-11-17 18:13:12 -0800

    Adds test for exam score insertion and retrieval
    Test passes

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -35,4 +35,31 @@ class StudentTest {
 
         assertStudentAssignmentScoresAreCorrect(student, assignmentScores);
     }
+
+    public void assertStudentExamScoresAreCorrect(Student student,
+                                                        ArrayList<Double> examScore) {
+        ArrayList<Double> studentScores = new ArrayList<>();
+        for(Double x: student.getExamsScores()) {
+            studentScores.add(x);
+        }
+        studentScores.sort(Double::compareTo);
+        assertArrayEquals(studentScores.toArray(), examScore.toArray());
+    }
+
+    @Test
+    public void test_examsCanBeAddedAndRetrieved() {
+        ArrayList<Double> assignmentScores = new ArrayList<>();
+        assignmentScores.add(21.0);
+        assignmentScores.add(10.1);
+
+        assignmentScores.sort(Double::compareTo);
+
+        Student student = new Student("test");
+
+        for(Double x: assignmentScores) {
+            student.addExamScore(x);
+        }
+
+        assertStudentExamScoresAreCorrect(student, assignmentScores);
+    }
 }
