c82bc5c22c30e7bd0371bd71f6ba737f98a7055f
john <johnotters@yahoo.com>
2019-11-17 19:26:27 -0800

    Refactor test code into methods

--- test/WeightedAverageCalculatorTest.java
+++ test/WeightedAverageCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -5,36 +6,40 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeightedAverageCalculatorTest {
+    @BeforeEach
+    public void setup() {
+        student = new Student("test");
+        examScores = new ArrayList<>();
+        assignmentScores = new ArrayList<>();
+    }
+
     @Test
     public void test_correctlyCalculatesAverage() {
-        //exams scores
-        ArrayList<Double> examScores = new ArrayList<>();
-        examScores.add(21.0);
-        examScores.add(10.1);
+        addExamScores(21.0, 10.1);
 
-        examScores.sort(Double::compareTo);
-
-        Student student = new Student("test");
-
-        for(Double x: examScores) {
-            student.addExamScore(x);
-        }
-
-
-        //assignment scores
-        ArrayList<Double> assignmentScores = new ArrayList<>();
-        assignmentScores.add(18.9);
-        assignmentScores.add(54.2);
-
-        assignmentScores.sort(Double::compareTo);
-
-        for(Double x: assignmentScores) {
-            student.addAssignmentScore(x);
-        }
-
-
+        addAssignmentScores(18.9, 54.2);
 
         WeightedAverageCalculator calculator = new WeightedAverageCalculator();
         assertEquals(23.95, calculator.calc(student.getAssignmentScores(), student.getExamsScores()));
     }
+
+    public void addAssignmentScores(double... scores) {
+        for(double score: scores) {
+            assignmentScores.add(score);
+            student.addAssignmentScore(score);
+        }
+        assignmentScores.sort(Double::compareTo);
+    }
+
+    public void addExamScores(double... scores) {
+        for(double score: scores) {
+            examScores.add(score);
+            student.addExamScore(score);
+        }
+        examScores.sort(Double::compareTo);
+    }
+
+    private Student student;
+    private ArrayList<Double> examScores;
+    private ArrayList<Double> assignmentScores;
 }
