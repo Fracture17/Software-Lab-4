9051e6cfcd6f751e7491fad25eaead5e2f39c286
john <johnotters@yahoo.com>
2019-11-17 18:11:22 -0800

    Adds test for assignment score insertion and retrieval
    Test passes

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,5 +1,38 @@
+import org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudentTest {
 
+class StudentTest {
+    public void assertStudentAssignmentScoresAreCorrect(Student student,
+                                                            ArrayList<Double> assignmentScores) {
+        ArrayList<Double> studentScores = new ArrayList<>();
+        for(Double x: student.getAssignmentScores()) {
+            studentScores.add(x);
+        }
+        studentScores.sort(Double::compareTo);
+        assertArrayEquals(studentScores.toArray(), assignmentScores.toArray());
+    }
+
+    @Test
+    public void test_assignmentsCanBeAddedAndRetrieved() {
+        ArrayList<Double> assignmentScores = new ArrayList<>();
+        assignmentScores.add(21.0);
+        assignmentScores.add(10.1);
+
+        assignmentScores.sort(Double::compareTo);
+
+        Student student = new Student("test");
+
+        for(Double x: assignmentScores) {
+            student.addAssignmentScore(x);
+        }
+
+        assertStudentAssignmentScoresAreCorrect(student, assignmentScores);
+    }
 }
