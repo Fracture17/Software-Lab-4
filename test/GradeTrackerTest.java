02228b74647073ba0050ec27b1125b826c7eaa2e
john <johnotters@yahoo.com>
2019-11-20 21:38:32 -0800

    Implements GradeTracker test

--- test/GradeTrackerTest.java
+++ test/GradeTrackerTest.java
@@ -1,10 +1,45 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+//import org.junit.jupiter.api.Test;
 
-class GradeTrackerTest {
+import java.util.ArrayList;
+import org.junit.Assert.*;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GradeTrackerTest {
     @Test
     public void test_gradeTrackerAutomaticallyUpdates() {
-        
+        Student student = new Student("test");
+
+        ArrayList<Double> examScores = new ArrayList<>();
+        examScores.add(50.9);
+        examScores.add(33.3);
+
+        for(Double x: examScores) {
+            student.addExamScore(x);
+        }
+
+        ArrayList<Double> assignmentScores = new ArrayList<>();
+        assignmentScores.add(94.8);
+        assignmentScores.add(63.7);
+        assignmentScores.add(55.0);
+
+        for(Double x: assignmentScores) {
+            student.addAssignmentScore(x);
+        }
+
+        GradeTracker tracker = new GradeTracker(student, new WeightedAverageCalculator());
+        assertEquals("F", tracker.getLetterGrade());
+
+        student.addExamScore(100);
+        assertEquals("D", tracker.getLetterGrade());
     }
 }
