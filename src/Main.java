1bcd972001ec8dbcda88045198e473b8884038dc
john <johnotters@yahoo.com>
2019-11-20 21:58:59 -0800

    Adds showTracker method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         showCalculators();
+        System.out.println();
+
+        showTracker();
     }
 
     public static void showCalculators() {
@@ -21,6 +24,25 @@ public class Main {
 
         System.out.println("Method A expected: " + 43.826);
         System.out.println("Method A actual: " + student.calcClassAverage(calculator));
+    }
+
+    public static void showTracker() {
+        Student student = new Student("test");
+
+        addExamScores(student, 50.9, 33.3);
+        addAssignmentScores(student, 94.8, 63.7, 50.0);
+
+        GradeTracker tracker = new GradeTracker(student, new WeightedAverageCalculator());
+
+        System.out.println("Class Average: " + student.calcClassAverage(new WeightedAverageCalculator()));
+        System.out.println("Tracker expected: F");
+        System.out.println("Tracker actual: " + tracker.getLetterGrade());
+
+        student.addExamScore(100.0);
+        System.out.println("Add exam score of 100.0");
+        System.out.println("New score: " + student.calcClassAverage(new WeightedAverageCalculator()));
+        System.out.println("Tracker expected: D");
+        System.out.println("Tracker actual: " + tracker.getLetterGrade());
     }
 
     public static void addExamScores(Student student, double... scores) {
